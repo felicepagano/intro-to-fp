b1ef48c8f933e333402c100f729dba6c0c986844
Felice Pagano <paganofelice@gmail.com>
2018-05-04 23:26:45 +0200

    implementa un bifuntore di Tuple

--- src/4.2.ts
+++ src/4.2.ts
@@ -2,6 +2,7 @@ type Tuple<A, B> = [A, B];
 
 const map = <A, B, C>(f: (a: A) => C, value: Tuple<A, B>): Tuple<C, B> => [f(value[0]), value[1]];
 const map2 = <A, B, C>(f: (a: B) => C, value: Tuple<A, B>): Tuple<A, C> => [value[0], f(value[1])];
+const bimap = <L, A, M, B>(fla: Tuple<L, A>, f: (l: L) => M, g: (a: A) => B): Tuple<M, B> => [f(fla[0]), g(fla[1])];
 
 const double = (i: number) => i * 2;
 const l = (s: string) => s.length;
@@ -13,6 +14,15 @@ describe("Tuple functor", () => {
 
   it("should return [5,2]", () => {
     const result = map2(double, map(l, ["Pippo", 1]));
+    expect(result).to.eql([5, 2]);
+  });
+
+});
+
+describe("Tuple bifunctor", () => {
+
+  it("should return [5,2]", () => {
+    const result = bimap(["Pippo", 1], l, double);
     expect(result).to.eql([5, 2]);
   });
 

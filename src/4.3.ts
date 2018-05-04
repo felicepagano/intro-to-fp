24e6bf7bd1856bc7d887c3b7808b447661ccb21b
Felice Pagano <paganofelice@gmail.com>
2018-05-04 22:39:47 +0200

    implementa il funtore di reader

--- src/4.3.ts
+++ src/4.3.ts
@@ -1,8 +1,9 @@
 type Decoder<A> = (s: string) => A;
+type Reader<E, A> = (e: E) => A;
 
-const map = <A, B>(f: (a: A) => B, value: Decoder<A>): Decoder<B> => {
-    return (s: string) => f(value(s));
-};
+const map = <A, B>(f: (a: A) => B, value: Decoder<A>): Decoder<B> => (s: string) => f(value(s));
+
+const map2 = <E, A, B>(f: (a: A) => B, value: Reader<E, A>): Reader<E, B> => (e: E): B => f(value(e));
 
 import { expect } from "chai";
 import "mocha";
@@ -10,9 +11,16 @@ import "mocha";
 describe("Decoder functor", () => {
 
   const decoder: Decoder<number> = (s: string) => s.length;
+  const reader: Reader<string, number> = (s: string) => s.length;
+  const Ff = (i: number) => i * i;
 
-  it("should return 25", () => {
-    const result = map((i: number) => i * i, decoder)("pippo");
+  it("should return 25 for map", () => {
+    const result = map(Ff, decoder)("pippo");
+    expect(result).to.equal(25);
+  });
+
+  it("should return 25 for map2", () => {
+    const result = map2(Ff, decoder)("pippo");
     expect(result).to.equal(25);
   });
 
